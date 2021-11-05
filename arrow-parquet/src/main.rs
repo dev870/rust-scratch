1b139a47751b4858f23a51624852cbc20f6f91d9
dev870 <developershmail@gmail.com>
2021-11-05 18:13:30 +0530

    add recordbatch concat example

--- arrow-parquet/src/main.rs
+++ arrow-parquet/src/main.rs
@@ -4,6 +4,7 @@ use std::io::{BufReader, SeekFrom, Seek};
 use arrow::json::reader::infer_json_schema;
 use std::sync::Arc;
 use arrow::json;
+use arrow::record_batch::RecordBatch;
 use parquet::arrow::arrow_writer::ArrowWriter;
 use parquet::file::properties::WriterProperties;
 use parquet::file::reader::SerializedFileReader;
@@ -14,39 +15,53 @@ fn main() {
         let mut reader = BufReader::new(&file);
         let inferred_schema = infer_json_schema(&mut reader, None).unwrap();
         file.seek(SeekFrom::Start(0)).unwrap();
-
         let mut json = json::Reader::new(BufReader::new(file), Arc::new(inferred_schema), 1024, None);
         let batch = json.next().unwrap().unwrap();
-        println!("{}", batch.num_columns());
-        println!("{}", batch.num_rows());
+        
+        let mut file2 = File::open("./data/input2.json").unwrap();
+        let mut reader2 = BufReader::new(&file2);
+        let inferred_schema2 = infer_json_schema(&mut reader2, None).unwrap();
+        file2.seek(SeekFrom::Start(0)).unwrap();
+        let mut json2 = json::Reader::new(BufReader::new(file2), Arc::new(inferred_schema2), 1024, None);
+        let batch2 = json2.next().unwrap().unwrap();
+        
+        file2 = File::open("./data/input2.json").unwrap();
+        let mut reader2 = BufReader::new(&file2);
+        let inferred_schema3 = infer_json_schema(&mut reader2, None).unwrap();
+        file2.seek(SeekFrom::Start(0)).unwrap();     
+        let vec = vec![batch, batch2];
 
-        let parquet_file = File::create("./data/data.parquet").unwrap();
-        let mut file1 = File::open("./data/input1.json").unwrap();
-        let mut reader1 = BufReader::new(&file1);
-        let inferred_schema_clone = infer_json_schema(&mut reader1, None).unwrap();
-        let props = WriterProperties::builder().build();
-        let mut writer = ArrowWriter::try_new(parquet_file, Arc::new(inferred_schema_clone), Some(props)).unwrap();
-        writer.write(&batch).expect("Writing batch");
-        writer.close().unwrap();
+        let new_batch = RecordBatch::concat(&Arc::new(inferred_schema3), &vec).unwrap();
+        println!("{}", new_batch.num_columns());
+        println!("{}", new_batch.num_rows());
 
-        let file = File::open("./data/data.parquet").unwrap();
-        let file_reader = SerializedFileReader::new(file).unwrap();
-        let mut arrow_reader = ParquetFileArrowReader::new(Arc::new(file_reader));
+        // let parquet_file = File::create("./data/data.parquet").unwrap();
+        // let mut file1 = File::open("./data/input1.json").unwrap();
+        // let mut reader1 = BufReader::new(&file1);
+        // let inferred_schema_clone = infer_json_schema(&mut reader1, None).unwrap();
+        // let props = WriterProperties::builder().build();
+        // let mut writer = ArrowWriter::try_new(parquet_file, Arc::new(inferred_schema_clone), Some(props)).unwrap();
+        // writer.write(&batch).expect("Writing batch");
+        // writer.close().unwrap();
+
+        // let file = File::open("./data/data.parquet").unwrap();
+        // let file_reader = SerializedFileReader::new(file).unwrap();
+        // let mut arrow_reader = ParquetFileArrowReader::new(Arc::new(file_reader));
  
-        println!("Converted arrow schema is: {}", arrow_reader.get_schema().unwrap());
-        println!("Arrow schema after projection is: {}",
-        arrow_reader.get_schema_by_columns(vec![0], true).unwrap());
+        // println!("Converted arrow schema is: {}", arrow_reader.get_schema().unwrap());
+        // println!("Arrow schema after projection is: {}",
+        // arrow_reader.get_schema_by_columns(vec![0], true).unwrap());
 
-        let mut record_batch_reader = arrow_reader.get_record_reader(2048).unwrap();
+        // let mut record_batch_reader = arrow_reader.get_record_reader(2048).unwrap();
 
-        for maybe_record_batch in record_batch_reader {
-            let record_batch = maybe_record_batch.unwrap();
-                if record_batch.num_rows() > 0 {
-                    println!("Read {} records.", record_batch.num_rows());
-                } else {
-                    println!("End of file!");
-                }
-                println!("{}", record_batch.num_columns());
-                println!("{}", record_batch.num_rows());
-        }
+        // for maybe_record_batch in record_batch_reader {
+        //     let record_batch = maybe_record_batch.unwrap();
+        //         if record_batch.num_rows() > 0 {
+        //             println!("Read {} records.", record_batch.num_rows());
+        //         } else {
+        //             println!("End of file!");
+        //         }
+        //         println!("{}", record_batch.num_columns());
+        //         println!("{}", record_batch.num_rows());
+        // }
 }
